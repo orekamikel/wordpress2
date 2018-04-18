70f9706fabe6d33ccd7d7de167b4e53a00a2bb8c
MikelOreka <38464334+MikelOreka@users.noreply.github.com>
2018-04-18 09:42:54 +0200

    Add files via upload

--- mitema/index.php
+++ mitema/index.php
@@ -2,36 +2,38 @@
 
 
 <section id="main">
+    <div id="section-post">
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                <div class="post-index">
 
-    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-    <div class="post-index">
+                    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                    <small>Publicado el <?php the_time('j/m/Y') ?> por <?php the_author_posts_link() ?>  </small>
+                    <div class="thumbnail">
+                        <?php
+                        if (has_post_thumbnail()) {
+                            the_post_thumbnail();
+                        }
+                        ?>
+                    </div>
+                    <?php the_excerpt(); ?>
+                </div>
 
-            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-            <small>Publicado el <?php the_time('j/m/Y') ?> por <?php the_author_posts_link() ?>  </small>
-            <div class="thumbnail">
-                <?php
-                if (has_post_thumbnail()) {
-                    the_post_thumbnail();
-                }
-                ?>
-            </div>
-            <?php the_excerpt(); ?>
-            </div>
-    
-    <hr>
-        <?php endwhile;
-    else:
-        ?>
-        <p><?php _e('No hay entradas .'); ?></p>
+                <hr>
+            <?php
+            endwhile;
+        else:
+            ?>
+            <p><?php _e('No hay entradas .'); ?></p>
 <?php endif; ?>
+    </div>
 
 
 
-
+<?php get_sidebar() ?>
 
 </section> <!-- Fin de main -->
 
-<?php get_sidebar() ?>
+
 
 
 <?php get_footer(); ?>
